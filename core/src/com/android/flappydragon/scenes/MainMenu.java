7bd8e226b47f16ba7a6cab172562220af8fbbe84
RodrigoRibeiroSilva <32637986+RodrigoRibeiroSilva@users.noreply.github.com>
2018-05-26 18:35:39 -0300

    implementada a tela de login e o webservice

---
+++ core/src/com/android/flappydragon/scenes/MainMenu.java
@@ -0,0 +1,8 @@
+package com.android.flappydragon.scenes;
+
+/**
+ * Created by Rodrigo on 23/05/2018.
+ */
+
+public class MainMenu {
+}
---
+++ core/src/com/android/flappydragon/webservice/HttpRequest.java
@@ -0,0 +1,8 @@
+package com.android.flappydragon.webservice;
+
+/**
+ * Created by Rodrigo on 26/05/2018.
+ */
+
+public class HttpRequest {
+}
